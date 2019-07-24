c2a50194d3c64f656266ed95f31709422d17266a
ReplacedToy <barry15321@gmail.com>
2019-07-24 22:59:15 +0800

    0724 test() function

--- SummonersWar/AutoClick.cs
+++ SummonersWar/AutoClick.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -36,21 +39,24 @@ namespace SummonersWar
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
             sw.Reset();
             sw.Start();
-            Image img = Image.FromFile(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\ms.jpg");
-            Bitmap s = (Bitmap)img;
-            for (int i = 0; i < s.Width; i++)
-            {
-                for (int j = 0; j < s.Height; j++)
-                {
-                    Color pixel = s.GetPixel(i, j);
-                    Console.WriteLine("i = " + i.ToString() + " , j = " + j.ToString() + " , " + pixel.ToArgb()
-                        + " , A : " + pixel.A + " R : " + pixel.R + " G : " + pixel.G + " B : " + pixel.B);
-                }
-            }
 
-            Console.WriteLine("Img Size : " + s.Width + " * " + s.Height + "( wid * hei )");
-            sw.Stop();
-            Console.WriteLine("done. Time spend : " + sw.Elapsed.TotalMilliseconds.ToString() + "ms .");
+            test();
+            //Image img = Image.FromFile(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Source.png");
+            //Bitmap s = (Bitmap)img;
+
+            //for (int i = 0; i < s.Width; i++)
+            //{
+            //    for (int j = 0; j < s.Height; j++)
+            //    {
+            //        Color pixel = s.GetPixel(i, j);
+            //        //Console.WriteLine("i = " + i.ToString() + " , j = " + j.ToString() + " , " + pixel.ToArgb()
+            //        //    + " , A : " + pixel.A + " R : " + pixel.R + " G : " + pixel.G + " B : " + pixel.B);
+            //    }
+            //}
+
+            //Console.WriteLine("Img Size : " + s.Width + " * " + s.Height + " ( wid * hei )");
+            //sw.Stop();
+            //Console.WriteLine("done. Time spend : " + sw.Elapsed.TotalMilliseconds.ToString() + "ms .");
         }
 
         private void summonerWarToolStripMenuItem_Click(object sender, EventArgs e)
@@ -86,6 +92,110 @@ namespace SummonersWar
             Image s = cap.CapturehWndWindow(AssistantHwnd);
             s.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss") + "_TestImage.png");
             //Set Timer , CaptureScreen , ToSearch pixel
+        }
+
+        private void test()
+        {
+            Bitmap bmpGray = new Bitmap(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Source.png");
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < bmpGray.Height; i++)
+            {
+                for (int j = 0; j < bmpGray.Width; j++)
+                {
+                    Color color = bmpGray.GetPixel(j, i);
+                    //int aver = (color.R + color.G + color.B) / 3;
+                    int aver = (color.R * 38 + color.G * 75 + color.B * 15) >> 7;
+                    bmpGray.SetPixel(j, i, Color.FromArgb(aver, aver, aver));
+                }
+            }
+            bmpGray.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\WhatDoesImageDo.png");
+            stopwatch.Stop();
+
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch2.Start();
+
+            bmpGray = new Bitmap(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Source.png");
+            Rectangle rect = new Rectangle(1, 1, bmpGray.Width - 2, bmpGray.Height - 2);
+            BitmapData bmpData = bmpGray.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            IntPtr ptr = bmpData.Scan0;
+            for (int i = 0; i < rect.Height; i++)
+            {
+                for (int j = 0; j < rect.Width; j++)
+                {
+                    int aver = 0;
+                    for (int k = 0; k < 3; k++)
+                    {
+                        aver += Marshal.ReadByte(ptr, i * bmpData.Stride + j * 3 + k);
+                        //Marshal.ReadByte(ptr, i * bmpData.Stride + j * 3 + k); = pixel
+
+                    }
+                    aver /= 3;
+                    for (int k = 0; k < 3; k++)
+                    {
+                        Marshal.WriteByte(ptr, i * bmpData.Stride + j * 3 + k, (byte)aver);
+                    }
+                }
+            }
+            bmpGray.UnlockBits(bmpData);
+            bmpGray.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\WhatDoesImageDo2.png");
+            stopwatch2.Stop();
+
+            Stopwatch stopwatch3 = new Stopwatch();
+            stopwatch3.Start();
+            bmpGray = new Bitmap(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Source.png");
+
+            //Rectangle rect2 = new Rectangle(1, 1, bmpGray.Width - 2, bmpGray.Height - 2);
+            Rectangle rect2 = new Rectangle(0, 0, bmpGray.Width - 1 , bmpGray.Height - 1);
+            BitmapData bmpData2 = bmpGray.LockBits(rect2, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            IntPtr ptr2 = bmpData2.Scan0;
+            byte[] rgbValues = new byte[rect2.Height * rect2.Width * 3];
+            Marshal.Copy(ptr2, rgbValues, 0, rgbValues.Length);
+            for (int i = 0; i < rgbValues.Length; i += 3)
+            {
+                int colorTemp = rgbValues[i] + rgbValues[i + 1] + rgbValues[i + 2];
+                colorTemp /= 3;
+                rgbValues[i] = rgbValues[i + 1] = rgbValues[i + 2] = (byte)colorTemp;
+            }
+            Marshal.Copy(rgbValues, 0, ptr2, rgbValues.Length);
+            bmpGray.UnlockBits(bmpData);
+            bmpGray.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\WhatDoesImageDo3.png");
+            stopwatch3.Stop();
+
+            Stopwatch stopwatch4 = new Stopwatch();
+            stopwatch4.Start();
+
+            bmpGray = new Bitmap(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Source.png");
+            Rectangle rect4 = new Rectangle(1, 1, bmpGray.Width - 2, bmpGray.Height - 2);
+            BitmapData bmpData4 = bmpGray.LockBits(rect4, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            unsafe
+            {
+                byte* ptr4 = (byte*)(bmpData4.Scan0);
+                for (int i = 0; i < rect4.Height; i++)
+                {
+                    for (int j = 0; j < rect4.Width; j++)
+                    {
+                        int aver = 0;
+                        for (int k = 0; k < 3; k++)
+                        {
+                            aver += ptr4[i * bmpData4.Stride + j * 3 + k];
+                        }
+                        aver /= 3;
+                        for (int k = 0; k < 3; k++)
+                        {
+                            ptr4[i * bmpData4.Stride + j * 3 + k] = (byte)aver;
+                        }
+                    }
+                }
+            }
+            bmpGray.UnlockBits(bmpData4);
+            bmpGray.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\WhatDoesImageDo4.png");
+            stopwatch4.Stop();
+
+            Console.WriteLine("StopWatch : " + stopwatch.Elapsed.TotalMilliseconds.ToString() + "ms .");
+            Console.WriteLine("StopWatch2 : " + stopwatch2.Elapsed.TotalMilliseconds.ToString() + "ms .");
+            Console.WriteLine("StopWatch3 : " + stopwatch3.Elapsed.TotalMilliseconds.ToString() + "ms .");
+            Console.WriteLine("StopWatch4 : " + stopwatch4.Elapsed.TotalMilliseconds.ToString() + "ms .");
         }
     }
 }
